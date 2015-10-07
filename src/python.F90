b84102f1d26d9434baa790ba90d0926813229ab7
Bertrand Putigny <bertrand.putigny@irap.omp.eu>
2015-10-07 11:10:47 +0200

    Python interface: add get_grid function

--- src/python.F90
+++ src/python.F90
@@ -18,4 +18,36 @@ contains
         vecps = vec
     end subroutine
 
+    subroutine get_nr(nr)
+        use mod_grid, only: ndomains, grd
+        implicit none
+        integer, intent(out) :: nr
+
+        integer :: id
+
+        nr = 0
+        do id=1, ndomains
+            nr = nr + grd(id)%nr
+        enddo
+
+    end subroutine
+
+    subroutine get_grid(nr, grid)
+        use mod_grid, only: ndomains, grd
+        implicit none
+        integer, intent(in) :: nr
+        double precision, intent(out) :: grid(nr)
+
+        integer :: ir, id
+
+        ir = 1
+        do id=1, ndomains
+            print*, "dom: ", id, "size: ", grd(id)%nr
+            print*, "write: ", ir, ir+grd(id)%nr-1
+            grid(ir:ir+grd(id)%nr-1) = grd(id)%r(:)
+            ir = ir + grd(id)%nr
+        enddo
+
+    end subroutine
+
 end module python
