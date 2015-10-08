top-devel/top

commit 16d76a7e94e2646705e93c2e877aea3fcae401d2

Check version compatibility
Check version compatibility between python module and compiled library

configure.ac
@@ -2,7 +2,9 @@
 # Process this file with autoconf to produce a configure script.
 
 AC_PREREQ([2.59])
-AC_INIT([TOP], [0.8])
+m4_include([version.m4])
+AC_INIT([TOP],
+        TOP_VERSION)
 AC_CONFIG_AUX_DIR(autoconf)
 AC_CONFIG_MACRO_DIR([m4])
 
@@ -112,7 +114,6 @@ AC_CONFIG_FILES([Makefile
                  lib/itemlist/Makefile
                  lib/lapack_extra/Makefile
                  utils/Makefile
-                 utils/top-build
                  python/Makefile
                  examples/Makefile
                  utils/readeq-real-2D-mono
@@ -125,7 +126,11 @@ AC_CONFIG_FILES([Makefile
                  utils/readeq-cplx-2D-multi
                  src/Makefile])
 
+AM_ENABLE_SHARED
+AM_DISABLE_STATIC
+
 AC_OUTPUT
+
 
 # 
 # --------------------------------------------------------------------------------
@@ -145,4 +150,5 @@ cat << EOF
 prefix:     $prefix
 LDLFAGS:    $LDFLAGS
 LIBS:       $LIBS
+VERSION:    $VERSION
 EOF

python/Makefile.am
@@ -1,14 +1,18 @@
 do_subst = sed -e 's,[@]datadir[@],$(datadir),g' \
                -e 's,[@]prefix[@],$(prefix),g'   \
                -e 's,[@]abs_top_builddir[@],$(abs_top_builddir),g'   \
+               -e 's,[@]VERSION[@],$(VERSION),g' \
                -e 's,[@]PYTHON[@],$(PYTHON),g'
+
+EXTRA_DIST = top.in top-build.in
+BUILT_SOURCES = top.py top-build.py
 
 bin_SCRIPTS = top.py
 
-BUILT_SOURCES = top.py
-
-.in.py: Makefile
+top-build.py: top-build.in Makefile
 	$(do_subst) < $< > $@
 
-clean-local:
-	rm -f $(BUILT_SOURCES)
+top.py: top.in Makefile
+	$(do_subst) < $< > $@
+
+CLEANFILES = $(BUILT_SOURCES)

python/top.in
@@ -5,10 +5,9 @@ import shutil as sh
 import ctypes as ct
 import numpy as np
 
-__all__ = ['load', 'read_dati', 'run_arncheb', 'get_valps', 'get_vecps',
-        'init_model', 'get_from_dati', 'get_grid']
+version = "@VERSION@"
 
-def error(msg):
+def _error(msg):
     print('[error]: ' + msg)
 
 def get_from_dati(key):
@@ -46,11 +45,12 @@ def load(name):
     global lib_get_vecps
     global lib_get_nr
     global lib_get_grid
+    global lib_get_version
 
     lib = '@abs_top_builddir@/src/lib/' + name + '.so'
     if not os.path.isfile(lib):
-        error('no such file: ' + lib)
-        error('did you run top-build?')
+        _error('no such file: ' + lib)
+        _error('did you run top-build?')
         libtop = None
         return
     libtop = ct.cdll.LoadLibrary(lib)
@@ -65,6 +65,25 @@ def load(name):
     lib_get_vecps       = libtop.__python_MOD_get_vecps
     lib_get_nr          = libtop.__python_MOD_get_nr
     lib_get_grid        = libtop.__python_MOD_get_grid
+    lib_get_version     = libtop.__python_MOD_get_version
+    lib_version = get_version()
+    if lib_version != version:
+        print('[warning] - top module version: ' + version)
+        print('          - library version:    ' + lib_version)
+        print('         version mismatch, you could experience troubles.')
+        print('         Try re-building your equation file.')
+
+def get_version():
+    """
+    Get loaded library TOP version
+    """
+    if libtop == None:
+        _error('no library loaded')
+        return
+
+    buf = ct.create_string_buffer(256)
+    lib_get_version(ct.byref(buf), ct.c_int(255))
+    return buf.value.strip()
 
 def read_dati(dati):
     """
@@ -75,21 +94,21 @@ def read_dati(dati):
     global datifile
 
     if libtop == None:
-        error('no library loaded')
+        _error('no library loaded')
         return
     datifile = dati
     if not type(dati) is str:
-        error('init takes a filename as parameter')
+        _error('init takes a filename as parameter')
         return
 
-    buf = ct.create_string_buffer(len(dati)+1)
+    buf = ct.create_string_buffer(len(dati))
     buf.value = dati
 
-    lib_read_inputs(ct.byref(buf), ct.c_int(len(dati)+1))
+    lib_read_inputs(ct.byref(buf), ct.c_int(len(dati)))
 
-def get_ndomains():
+def _get_ndomains():
     if libtop == None:
-        error('No library loaded')
+        _error('No library loaded')
         return
     return int(ct.c_int.in_dll(libtop, "__mod_grid_MOD_ndomains").value)
 
@@ -100,31 +119,31 @@ def run_arncheb(shift):
     :param float shift: Initial shift
     """
     if libtop == None:
-        error('No library loaded')
+        _error('No library loaded')
         return
     if not type(shift) is float:
-        error('shift should be a float')
+        _error('shift should be a float')
         return
     lib_init_a()
     lib_init_order()
     lib_init_bc_flag()
     lib_run_arncheb(ct.byref(ct.c_double(shift)))
 
-def get_nsol_out():
+def _get_nsol_out():
     if libtop == None:
-        error('No library loaded')
+        _error('No library loaded')
         return
     return int(ct.c_int.in_dll(libtop, "__eigensolve_MOD_nsol_out").value)
 
-def get_nsol():
+def _get_nsol():
     if libtop == None:
-        error('No library loaded')
+        _error('No library loaded')
         return
     return int(ct.c_int.in_dll(libtop, "__inputs_MOD_nsol").value)
 
-def get_a_dim():
+def _get_a_dim():
     if libtop == None:
-        error('No library loaded')
+        _error('No library loaded')
         return
     return int(ct.c_int.in_dll(libtop, "__matrices_MOD_a_dim").value)
 
@@ -136,9 +155,9 @@ def get_valps():
     :return: An array of eigenvalue
     """
     if libtop == None:
-        error('No library loaded')
+        _error('No library loaded')
         return
-    n = get_nsol_out()
+    n = _get_nsol_out()
     valps = np.zeros(shape=(n), dtype=np.double)
     lib_get_valps(valps.ctypes.data)
     return valps
@@ -151,10 +170,10 @@ def get_vecps():
     :return: An array of eigenvector
     """
     if libtop == None:
-        error('No library loaded')
+        _error('No library loaded')
         return
-    n = get_nsol_out()
-    a_dim = get_a_dim()
+    n = _get_nsol_out()
+    a_dim = _get_a_dim()
     vecps = np.zeros(shape=(a_dim, n), dtype=np.double)
     lib_get_vecps(vecps.ctypes.data)
     return vecps
@@ -167,16 +186,16 @@ def init_model(filename):
     :param str filename: Filename or directory to read in order to initialize the model
     """
     if libtop == None:
-        error('No library loaded')
+        _error('No library loaded')
         return
     buf = ct.create_string_buffer(len(filename))
     buf.value = filename
 
     lib_init_model(ct.byref(buf), ct.c_int(len(buf)))
 
-def get_nr():
+def _get_nr():
     if libtop == None:
-        error('No library loaded')
+        _error('No library loaded')
         return
     nr = ct.c_int(0)
     lib_get_nr(ct.byref(nr))
@@ -190,9 +209,9 @@ def get_grid():
     :return: The grid of the model
     """
     if libtop == None:
-        error('No library loaded')
+        _error('No library loaded')
         return
-    nr = get_nr()
+    nr = _get_nr()
     grid = np.zeros(shape=(nr), dtype=np.double)
     lib_get_grid(ct.byref(ct.c_int(nr)), grid.ctypes.data)
     return grid

version.m4
@@ -1,0 +1,1 @@
+m4_define([TOP_VERSION], [0.9.2])

Makefile.am
@@ -1,3 +1,5 @@
 SUBDIRS = lib utils python examples
 
 EXTRA_DIST = src/Makefile.am src/Makefile.in
+
+DISTCLEANFILES = src/Makefile

src/Makefile.am
@@ -52,3 +52,4 @@ clean-local:
 	rm -f *.mod *.lo
 	rm -f inputs.F90 matrices.inc model.F90 mod_grid.F90
 
+DISTCLEANFILES = Makefile

utils/Makefile.am
@@ -1,5 +1,3 @@
-bin_SCRIPTS = top-build
-
 noinst_SCRIPTS = readeq-real-2D-mono \
 				 readeq-cplx-2D-mono \
 				 readeq-real-1D-mono \
@@ -9,7 +7,6 @@ noinst_SCRIPTS = readeq-real-2D-mono \
 				 readeq-real-2D-multi-mpi \
 				 readeq-cplx-2D-multi
 
-CLEANFILES = $(bin_SCRIPTS)
 DISTCLEANFILES = $(noinst_SCRIPTS)
 EXTRA_DIST = readeq-real-2D-mono \
 			 readeq-cplx-2D-mono \
@@ -19,7 +16,4 @@ EXTRA_DIST = readeq-real-2D-mono \
 			 readeq-cplx-2D-multi-mpi \
 			 readeq-real-2D-multi-mpi \
 			 readeq-cplx-2D-multi
-
-# do_subst = sed -e 's,[@]PERL[@],$(PERL),g' \
-# 		   -e 's,[@]IS_COMPLEX_TYPE[@],$(top_type),g'
 

examples/top-script.in
@@ -22,8 +22,7 @@ for v in valps:
 print('grid: ' + str(top.get_grid()))
 
 
-print()
-print('eq_ESTER_all_lagrange:')
+print('\neq_ESTER_all_lagrange:')
 top.load('eq_ESTER_all_lagrange')
 top.read_dati('@datadir@/top/models/ester/dati')
 model = top.get_from_dati('filename')

commit 7a2e7eb7a4c1e056b4909db88bc65e441b8c7d5b

Configure and build changes:
- Check python version since argparse is only available in python >=2.7
- Fix installation of top-build (bug introduced in 16d76a7e)

configure.ac
@@ -47,7 +47,7 @@ AC_PROG_F77($FC)
 AC_PROG_LIBTOOL
 AC_PATH_PROG(PERL, perl)
 
-AM_PATH_PYTHON
+AM_PATH_PYTHON([2.7])
 
 # Checks for libraries.
 AC_CHECK_LIB([m], [sqrt])
@@ -147,8 +147,13 @@ AC_OUTPUT
 # --------------------------------------------------------------------------------
 # 
 cat << EOF
-prefix:     $prefix
-LDLFAGS:    $LDFLAGS
-LIBS:       $LIBS
-VERSION:    $VERSION
+-------------------------------
+top version:    $VERSION
+python version: $PYTHON_VERSION
+install prefix: $prefix
+
+FC:             $FC
+LDLFAGS:        $LDFLAGS
+LIBS:           $LIBS
+-------------------------------
 EOF

python/Makefile.am
@@ -5,11 +5,11 @@ do_subst = sed -e 's,[@]datadir[@],$(datadir),g' \
                -e 's,[@]PYTHON[@],$(PYTHON),g'
 
 EXTRA_DIST = top.in top-build.in
-BUILT_SOURCES = top.py top-build.py
+BUILT_SOURCES = top.py top-build
 
-bin_SCRIPTS = top.py
+bin_SCRIPTS = top.py top-build
 
-top-build.py: top-build.in Makefile
+top-build: top-build.in Makefile
 	$(do_subst) < $< > $@
 
 top.py: top.in Makefile

python/top.in
@@ -5,10 +5,25 @@ import shutil as sh
 import ctypes as ct
 import numpy as np
 
-version = "@VERSION@"
+version = '@VERSION@'
 
 def _error(msg):
     print('[error]: ' + msg)
+
+def get_compiled_files():
+    """
+    Get the list of compiled equation files (that can be loaded with
+    :py:func:`load`)
+
+    :rtype: [str]
+    :return: List of compiled equation files
+    """
+    files = []
+    for dirpath, dirnames, filenames in os.walk('@abs_top_builddir@'):
+        for filename in filenames:
+            if not filename.startswith('lib') and filename.endswith('.so'):
+                files.append(filename[0:-3])
+    return files
 
 def get_from_dati(key):
     """
@@ -110,7 +125,7 @@ def _get_ndomains():
     if libtop == None:
         _error('No library loaded')
         return
-    return int(ct.c_int.in_dll(libtop, "__mod_grid_MOD_ndomains").value)
+    return int(ct.c_int.in_dll(libtop, '__mod_grid_MOD_ndomains').value)
 
 def run_arncheb(shift):
     """
@@ -133,19 +148,19 @@ def _get_nsol_out():
     if libtop == None:
         _error('No library loaded')
         return
-    return int(ct.c_int.in_dll(libtop, "__eigensolve_MOD_nsol_out").value)
+    return int(ct.c_int.in_dll(libtop, '__eigensolve_MOD_nsol_out').value)
 
 def _get_nsol():
     if libtop == None:
         _error('No library loaded')
         return
-    return int(ct.c_int.in_dll(libtop, "__inputs_MOD_nsol").value)
+    return int(ct.c_int.in_dll(libtop, '__inputs_MOD_nsol').value)
 
 def _get_a_dim():
     if libtop == None:
         _error('No library loaded')
         return
-    return int(ct.c_int.in_dll(libtop, "__matrices_MOD_a_dim").value)
+    return int(ct.c_int.in_dll(libtop, '__matrices_MOD_a_dim').value)
 
 def get_valps():
     """

version.m4
@@ -1,1 +1,1 @@
-m4_define([TOP_VERSION], [0.9.2])
+m4_define([TOP_VERSION], [0.6.0])