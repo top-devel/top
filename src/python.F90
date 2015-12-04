8fa7ea331bad06e70f6dfe24288fba811d28c6ad
Bertrand Putigny <bertrand.putigny@irap.omp.eu>
2015-12-04 21:41:44 +0100

    Add quick visualization function to python module

--- src/python.F90
+++ src/python.F90
@@ -235,23 +235,23 @@ end module modelpy
 
 
 module legpy
-    use fast_legendre
+    use fast_legendre, only: eval_ylm
     implicit none
 
 contains
-    subroutine eval(fs, nr, ns, nt, m, l, linc, f)
 
-        integer, intent(in) :: nr, ns, nt, l, linc, m
+    subroutine eval2d(fs, cost, l, linc, m, f, nr, ns, nt)
+
         real(kind=8), intent(in) :: fs(:, :)
+        real(kind=8), intent(in) :: cost(:)
+        integer, intent(in) :: nr, ns, nt, m, l, linc
         real(kind=8), intent(out) :: f(nr, nt)
+!f2py   integer intent(hide), depend(fs) :: nr=shape(fs, 0), ns=shape(fs, 1)
+!f2py   integer intent(hide), depend(cost) :: nt=shape(cost, 0)
 
-        ! local
-        double precision :: cth(nt), w(nt)
-
-        call gauleg(-1d0, 1d0, cth, w, nt)
         f = 0.0
-        call eval_ylm(1d0, fs, f, cth, nr, ns, nt, l, linc, m)
-    end subroutine eval
+        call eval_ylm(1d0, fs, f, cost, nr, ns, nt, l, linc, m)
+    end subroutine eval2d
 
     ! subroutine grid_to_spec()
     ! end subroutine grid_to_spec
