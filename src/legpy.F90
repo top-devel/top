a58688b61ef04dc2a5d304bf4cb1723e5e547e98
François Lignières <francois.lignieres@irap.omp.eu>
2016-03-31 11:58:32 +0200

    add theta derivative to legpy.f90

--- src/legpy.F90
+++ src/legpy.F90
@@ -1,5 +1,5 @@
 module legpy
-    use fast_legendre, only: eval_ylm
+    use fast_legendre	, only: eval_ylm, eval_dth_ylm
     implicit none
 
 contains
@@ -16,6 +16,19 @@ contains
         f = 0.0
         call eval_ylm(1d0, fs, f, cost, nr, ns, nt, l, linc, m)
     end subroutine eval2d
+
+    subroutine eval2d_dth(fs, cost, l, linc, m, f, nr, ns, nt)
+
+        real(kind=8), intent(in) :: fs(:, :)
+        real(kind=8), intent(in) :: cost(:)
+        integer, intent(in) :: nr, ns, nt, m, l, linc
+        real(kind=8), intent(out) :: f(nr, nt)
+!f2py   integer intent(hide), depend(fs) :: nr=shape(fs, 0), ns=shape(fs, 1)
+!f2py   integer intent(hide), depend(cost) :: nt=shape(cost, 0)
+
+        f = 0.0
+        call eval_dth_ylm(1d0, fs, f, cost, nr, ns, nt, l, linc, m)
+    end subroutine eval2d_dth
 
     ! subroutine grid_to_spec()
     ! end subroutine grid_to_spec
